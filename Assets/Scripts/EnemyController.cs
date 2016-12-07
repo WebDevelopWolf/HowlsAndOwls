750b60425bdd7bcb74f65cf7383546ed8a8bb8d9
Liane Stevenson <webdevelopwolf@gmail.com>
2016-12-07 22:46:49 +0000

    Enemy Behaviour
    Player can now destroy enemies with rocks

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+	
+	public float health = 150f;
+	
+	//Enemy hit with rock
+	void OnTriggerEnter2D(Collider2D col) {
+		Projectile rock = col.gameObject.GetComponent<Projectile>();
+		if (rock) {
+			//Remove Damage From Enemy Health
+			health -= rock.GetDamage();
+			rock.Hit();
+			//Kill Enemy if they run out of health
+			if (health <= 0) {
+				Destroy(gameObject);
+			}
+		}
+	}
+}
--- Assets/Scripts/Shredder.cs
+++ Assets/Scripts/Shredder.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 
 public class Shredder : MonoBehaviour {
-
+	
+	//Get rid of rocks not hitting enemies
 	void OnTriggerEnter2D(Collider2D col){
 		Destroy(col.gameObject);
 	}
