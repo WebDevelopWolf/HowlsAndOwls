0191513e9057588aa8718fd3ebf4814674c37739
Liane Stevenson <webdevelopwolf@gmail.com>
2016-12-12 20:06:27 +0000

    Music
    Added music for each screen

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -4,6 +4,12 @@ using System.Collections;
 public class MusicPlayer : MonoBehaviour {
 
 	static MusicPlayer instance = null;
+	
+	public AudioClip startClip;
+	public AudioClip gameClip;
+	public AudioClip endClip;
+	
+	private AudioSource music;
 
 	// Use this for initialization
 	void Start () {
@@ -14,7 +20,25 @@ public class MusicPlayer : MonoBehaviour {
 		else { 
 			instance = this;
 			GameObject.DontDestroyOnLoad(gameObject);
+			music = GetComponent<AudioSource>();
+			music.clip = startClip;
+			music.loop = true;
+			music.Play();
 		}
+	}
+	
+	void OnLevelWasLoaded(int level) {
+		Debug.Log("Music Player: loaded level " + level);
+		music.Stop();
+		if (level == 0) {
+			music.clip = startClip;
+		} else if (level == 1) {
+			music.clip = gameClip;
+		} else if (level == 2) {
+			music.clip = endClip;
+		}
+		music.loop = true;
+		music.Play();
 	}
 	
 	// Update is called once per frame
